cd4ba10be71200b230696dc94a6a38b257b74d62
birukbelaytafese <birukbelay175@gmail.com>
2026-05-19 12:28:13 -0500

    second commit

--- src/main/java/com/pluralsight/finance/Gold.java
+++ src/main/java/com/pluralsight/finance/Gold.java
@@ -1,4 +1,15 @@
 package com.pluralsight.finance;
 
-public class Gold {
+public class Gold extends FixedAsset {
+
+    private double weight;
+
+    public Gold(String name, double marketValue, double weight) {
+        super(name, marketValue);
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
 }
--- src/main/java/com/pluralsight/finance/House.java
+++ src/main/java/com/pluralsight/finance/House.java
@@ -1,4 +1,30 @@
 package com.pluralsight.finance;
 
-public class House {
+public class House extends FixedAsset {
+
+    private int yearBuilt;
+    private int squareFeet;
+    private int bedrooms;
+
+    public House(String name, double marketValue,
+                 int yearBuilt, int squareFeet, int bedrooms) {
+
+        super(name, marketValue);
+
+        this.yearBuilt = yearBuilt;
+        this.squareFeet = squareFeet;
+        this.bedrooms = bedrooms;
+    }
+
+    public int getYearBuilt() {
+        return yearBuilt;
+    }
+
+    public int getSquareFeet() {
+        return squareFeet;
+    }
+
+    public int getBedrooms() {
+        return bedrooms;
+    }
 }
