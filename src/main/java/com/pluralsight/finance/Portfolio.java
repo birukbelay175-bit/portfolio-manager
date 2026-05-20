627f7b0c0d93585f88b3c68d711fec980895b923
birukbelaytafese <birukbelay175@gmail.com>
2026-05-20 05:55:25 -0500

    part two Portfolio commit

---
+++ src/main/java/com/pluralsight/finance/Portfolio.java
@@ -0,0 +1,59 @@
+package com.pluralsight.finance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Portfolio {
+
+    private String name;
+    private String owner;
+    private List<Valuable> assets;
+
+    public Portfolio(String name, String owner) {
+
+        this.name = name;
+        this.owner = owner;
+
+        assets = new ArrayList<>();
+
+    }
+    public void add(Valuable asset) {
+        assets.add(asset);
+    }
+    public double getValue() {
+
+        double totalValue = 0;
+
+        for (Valuable asset : assets) {
+            totalValue += asset.getValue();
+        }
+
+        return totalValue;
+    }
+    public Valuable getMostValuable() {
+
+        Valuable mostValuable = assets.get(0);
+
+        for (Valuable asset : assets) {
+
+            if (asset.getValue() > mostValuable.getValue()) {
+                mostValuable = asset;
+            }
+        }
+
+        return mostValuable;
+    }
+    public Valuable getLeastValuable() {
+
+        Valuable leastValuable = assets.get(0);
+
+        for (Valuable asset : assets) {
+
+            if (asset.getValue() < leastValuable.getValue()) {
+                leastValuable = asset;
+            }
+        }
+
+        return leastValuable;
+    }
+}
